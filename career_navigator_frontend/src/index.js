796637165e60aac64b043d4f2203edafd81d110b
Kavia AI <production@kavia.ai>
2025-11-20 09:13:33 +0000

    Checkpoint 28 - cg043cfd12

--- career_navigator_frontend/src/index.js
+++ career_navigator_frontend/src/index.js
@@ -1,8 +1,16 @@
 /* Frontend entry point */
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./env-bootstrap";
+import "./index.css";
 import App from "./App";
 
 const el = document.getElementById("root");
 const root = createRoot(el);
-root.render(<App />);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
